61b3d4f541f98bf4df1fcf13dcbf0ad30cfff72c
Amanda So <soamanda95@gmail.com>
2023-05-11 17:55:39 -0700

    add prehook to hash new user password

--- models/user.js
+++ models/user.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const bcrypt = require("bcrypt");
+
 const UserSchema = new Schema({
   username: { type: String, required: true, maxLength: 75 },
   password: { type: String, required: true },
   photo: { type: Buffer },
   bio: { type: String, maxLength: 200 },
   friends: [{ type: Schema.Types.ObjectId, ref: "Friend" }],
+});
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
 });
 
 // Export model
