94adf29c0cd6254efec70ee1fb4b4d1b03dadf16
Amanda So <soamanda95@gmail.com>
2023-05-15 19:48:10 -0700

    add function to create new post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 
 exports.get_posts = (req, res, next) => {
   let isValid = validateObjectId(req.params.userid);
@@ -41,9 +45,50 @@ exports.get_post = (req, res, next) => {
     });
 };
 
-exports.create_post = (req, res, next) => {
-  res.send("TBD");
-};
+exports.create_post = [
+  // Validate and sanitize fields
+  body("text", "Post exceeds character limit.")
+    .trim()
+    .isLength({ max: 1000 })
+    .escape(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Errors exist. Send json with error messages
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Extract bearer token
+    let bearerToken = "";
+    const bearerHeader = req.headers.authorization;
+    bearerToken = extractBearerToken(bearerHeader);
+
+    // Verify Token
+    jwt.verify(bearerToken, process.env.jwt_key, (err, authData) => {
+      if (err) {
+        return next(err);
+      }
+      const post = new Post({
+        author: authData.user._id,
+        text: req.body.text,
+      })
+        .save()
+        .then(() => {
+          return res.json({
+            message: "New post created",
+            data: authData,
+          });
+        })
+        .catch((err) => {
+          return res.json({ error: err.message });
+        });
+    });
+  },
+];
 
 exports.edit_post = (req, res, next) => {
   res.send("TBD");
@@ -88,4 +133,15 @@ exports.delete_comment = (req, res, next) => {
 function validateObjectId(id) {
   isValid = mongoose.Types.ObjectId.isValid(id);
   return isValid;
+}
+
+function extractBearerToken(bearerHeader) {
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    bearerToken = bearer[1];
+    return bearerToken;
+  } else {
+    return bearerHeader;
+  }
 }
