409334add918b86c58648745d27d42db1932c060
Amanda So <soamanda95@gmail.com>
2023-05-15 20:14:10 -0700

    add function to edit a post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -90,9 +90,70 @@ exports.create_post = [
   },
 ];
 
-exports.edit_post = (req, res, next) => {
-  res.send("TBD");
-};
+exports.edit_post = [
+  // Validate and sanitize fields
+  body("text", "Post exceeds character limit.")
+    .trim()
+    .isLength({ max: 1000 })
+    .escape(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Errors exist. Send json with error messages
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check that post exists
+    let isValid = validateObjectId(req.params.postid);
+    if (!isValid) {
+      const error = new Error("Unable to update post.");
+      return next(error);
+    }
+    Post.findById(req.params.postid)
+      .select("author")
+      .then((result, err) => {
+        if (err) {
+          return next(err);
+        }
+        if (!result) {
+          const error = new Error("Post not found. Unable to update post.");
+          return next(error);
+        }
+        // Extract bearer token
+        let bearerToken = "";
+        const bearerHeader = req.headers.authorization;
+        bearerToken = extractBearerToken(bearerHeader);
+
+        // Verify Token
+        jwt.verify(bearerToken, process.env.jwt_key, (err, authData) => {
+          if (err) {
+            return next(err);
+          }
+          // current user id doesn't match post author id
+          if (authData.user._id !== result.author.toString()) {
+            const error = new Error("Not authorized.");
+            return next(error);
+          }
+
+          Post.findByIdAndUpdate(req.params.postid, {
+            text: req.body.text,
+          }).then((result, err) => {
+            if (err) {
+              return next(err);
+            }
+            return res.json({
+              message: "Post updated",
+              data: result,
+            });
+          });
+        });
+      });
+  },
+];
 
 exports.delete_post = (req, res, next) => {
   res.send("TBD");
