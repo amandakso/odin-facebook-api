5a6a2aa0324bf4f011cc7d2133da02849b4b9ed0
Amanda So <soamanda95@gmail.com>
2023-05-19 12:07:43 -0700

    add function to get user profile info

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,27 @@
+const mongoose = require("mongoose");
+User = require("../models/user");
+Friend = require("../models/friendship");
+
 exports.get_profile = (req, res, next) => {
-  res.send("TBD");
+  let isValid = validateObjectId(req.params.userid);
+  if (!isValid) {
+    const error = new Error("User not found.");
+    return next(error);
+  }
+  User.findById(req.params.userid)
+    .select("username photo bio friends")
+    .populate("friends")
+    .then((profile, err) => {
+      console.log(profile);
+      try {
+        if (err) {
+          return next(err);
+        }
+        return res.json(profile);
+      } catch (error) {
+        return next(error);
+      }
+    });
 };
 
 exports.get_friends = (req, res, next) => {
@@ -37,3 +59,19 @@ exports.update_username = (req, res, next) => {
 exports.update_pwd = (req, res, next) => {
   res.send("TBD");
 };
+
+function validateObjectId(id) {
+  isValid = mongoose.Types.ObjectId.isValid(id);
+  return isValid;
+}
+
+function extractBearerToken(bearerHeader) {
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    bearerToken = bearer[1];
+    return bearerToken;
+  } else {
+    return bearerHeader;
+  }
+}
